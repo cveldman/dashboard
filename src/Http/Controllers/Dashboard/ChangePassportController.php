4a94cc53439677aa45b17d59760cbbc62ab25b9b
Corné Veldman <cveldman@hotmail.com>
2020-06-24 16:16:59 +0200

    Update

---
+++ src/Http/Controllers/Dashboard/ChangePassportController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Veldman\Dashboard\Http\Controllers\Dashboard;
+
+use Veldman\Dashboard\Http\Controllers\Controller;
+
+class ChangePassportController extends Controller
+{
+    public function __construct()
+    {
+
+    }
+
+    public function index()
+    {
+
+    }
+}
---
+++ src/Http/Controllers/Dashboard/ChangeProfileController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Veldman\Dashboard\Http\Controllers\Dashboard;
+
+use Veldman\Dashboard\Http\Controllers\Controller;
+
+class ChangeProfileController extends Controller
+{
+    public function __construct()
+    {
+
+    }
+
+    public function index()
+    {
+
+    }
+}
