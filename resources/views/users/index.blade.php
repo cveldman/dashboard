c9f1c950b9462aedc5344b6d6cfcff8ae43a2998
Corné Veldman <cveldman@hotmail.com>
2020-07-01 13:05:41 +0200

    Update

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -12,49 +12,51 @@
         </div>
     </div>
 
-    <table class="table">
-        <thead>
-        <tr>
-            <th scope="col">#</th>
-            <th scope="col">{{ __('Name') }}</th>
-            <th scope="col">{{ __('Email') }}</th>
-            <th scope="col">{{ __('Roles') }}</th>
-            <th scope="col" class="text-right">{{ __('Actions') }}</th>
-        </tr>
-        </thead>
-        <tbody>
-        @if($items->isNotEmpty())
-            @foreach($items as $item)
-                <tr>
-                    <th scope="row">{{ $item->id }}</th>
-                    <td>
-                        @can('view', $item)
-                            <a href="{{ route('admin.users.show', $item) }}">
-                                {{ $item->name }}
-                            </a>
-                        @else
-                            {{ $item->name }}
-                        @endcan
-                    </td>
-                    <td>{{ $item->email }}</td>
-                    <td>{{ implode(' ', $item->roles->pluck('name')->all()) }}</td>
-                    <td class="text-right">
-                        @can('update', $item)
-                            <a href="{{ route('admin.users.edit', $item) }}">{{ __('Update') }}</a>
-                        @endcan
-
-                        @can('delete', $item)
-                            <a href="{{ route('admin.users.edit', $item) }}">{{ __('Delete') }}</a>
-                        @endcan
-                    </td>
-                </tr>
-            @endforeach
-        @else
+    <div class="row bg-white">
+        <table class="table">
+            <thead>
             <tr>
-                <td colspan="5" class="text-center">{{ __('No items') }}</td>
+                <th scope="col">#</th>
+                <th scope="col">{{ __('Name') }}</th>
+                <th scope="col">{{ __('Email') }}</th>
+                <th scope="col">{{ __('Roles') }}</th>
+                <th scope="col" class="text-right">{{ __('Actions') }}</th>
             </tr>
-        @endif
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+            @if($items->isNotEmpty())
+                @foreach($items as $item)
+                    <tr>
+                        <th scope="row">{{ $item->id }}</th>
+                        <td>
+                            @can('view', $item)
+                                <a href="{{ route('admin.users.show', $item) }}">
+                                    {{ $item->name }}
+                                </a>
+                            @else
+                                {{ $item->name }}
+                            @endcan
+                        </td>
+                        <td>{{ $item->email }}</td>
+                        <td>{{ implode(' ', $item->roles->pluck('name')->all()) }}</td>
+                        <td class="text-right">
+                            @can('update', $item)
+                                <a href="{{ route('admin.users.edit', $item) }}">{{ __('Update') }}</a>
+                            @endcan
+
+                            @can('delete', $item)
+                                <a href="{{ route('admin.users.edit', $item) }}">{{ __('Delete') }}</a>
+                            @endcan
+                        </td>
+                    </tr>
+                @endforeach
+            @else
+                <tr>
+                    <td colspan="5" class="text-center">{{ __('No items') }}</td>
+                </tr>
+            @endif
+            </tbody>
+        </table>
+    </div>
 
 @endsection
