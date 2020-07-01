9f2c5e4a128f77318f5ef7d05ee9cc0d3fa253d9
Corné Veldman <cveldman@hotmail.com>
2020-07-01 12:55:47 +0200

    Update

--- src/Policies/UserPolicy.php
+++ src/Policies/UserPolicy.php
@@ -10,41 +10,41 @@ class UserPolicy
 
     public function before($user, $ability)
     {
-        return true;
+        //
     }
 
     public function viewAny($user)
     {
-
+        return true;
     }
 
     public function view($user, $model)
     {
-        //
+        return false;
     }
 
     public function create($user)
     {
-        //
+        return true;
     }
 
     public function update($user, $model)
     {
-        //
+        return true;
     }
 
     public function delete($user, $model)
     {
-        //
+        return true;
     }
 
     public function restore($user, $model)
     {
-        //
+        return true;
     }
 
     public function forceDelete($user, $model)
     {
-        //
+        return true;
     }
 }
