0c1261ef1f9cd7f7acc2f015b46f604cb748de59
Corné Veldman <cveldman@hotmail.com>
2020-07-03 16:18:46 +0200

    Update

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -17,6 +17,10 @@
 
     <link rel="stylesheet" href="https://unpkg.com/@coreui/icons@2.0.0-beta.3/css/free.min.css">
 
+    <script
+            src="https://code.jquery.com/jquery-3.5.1.min.js"
+            integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+            crossorigin="anonymous"></script>
 
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/css/select2.min.css" rel="stylesheet" />
 
