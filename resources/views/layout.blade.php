547aba8b126aad3be63024c2c4476e9b7b09f822
Corné Veldman <cveldman@hotmail.com>
2020-07-06 12:42:09 +0200

    Update

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -17,21 +17,6 @@
 
     <link rel="stylesheet" href="https://unpkg.com/@coreui/icons@2.0.0-beta.3/css/free.min.css">
 
-    <script
-            src="https://code.jquery.com/jquery-3.5.1.min.js"
-            integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
-            crossorigin="anonymous"></script>
-
-    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/css/select2.min.css" rel="stylesheet" />
-
-    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script>
-
-    <script>
-        $(document).ready(function() {
-            $('.select2').select2();
-        });
-    </script>
-
 </head>
 <body>
 <div id="app" class="c-app">
