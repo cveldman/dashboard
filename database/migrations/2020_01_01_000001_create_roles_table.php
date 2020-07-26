719e7124cc60e03c2fe86a0dada4bfbcf1f3ec00
Corné Veldman <cveldman@hotmail.com>
2020-07-26 19:50:27 +0200

    Update 2020_01_01_000001_create_roles_table.php

--- database/migrations/2020_01_01_000001_create_roles_table.php
+++ database/migrations/2020_01_01_000001_create_roles_table.php
@@ -11,6 +11,7 @@ class CreateRolesTable extends Migration
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
             $table->text('name');
+            $table->text('description')->nullable();
             $table->timestamps();
         });
 
